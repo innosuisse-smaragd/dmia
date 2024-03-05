77d261a6c766fbe3202b2f7b560803331ed32e84
Thomas Jeanmonod <thomas.jeanmonod@mimacom.com>
2024-03-05 11:29:55 +0100

    Add spring boot as backend

---
+++ src/main/java/org/bfh/smaragd/dmia/DmiaApplication.java
@@ -0,0 +1,13 @@
+package org.bfh.smaragd.dmia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DmiaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DmiaApplication.class, args);
+    }
+
+}
