343a9c2923d498e58db8dbe1e2af25239fc66f96
Thomas Jeanmonod <thomas.jeanmonod@mimacom.com>
2024-03-07 10:11:47 +0100

    use variable Profiles.DEV to define profile in WebConfig

--- src/main/java/org/bfh/smaragd/dmia/controller/WebConfig.java
+++ src/main/java/org/bfh/smaragd/dmia/controller/WebConfig.java
@@ -1,12 +1,13 @@
 package org.bfh.smaragd.dmia.controller;
 
+import org.bfh.smaragd.dmia.Profiles;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-@Profile("dev")
+@Profile(Profiles.DEV)
 public class WebConfig implements WebMvcConfigurer {
 
     @Override
