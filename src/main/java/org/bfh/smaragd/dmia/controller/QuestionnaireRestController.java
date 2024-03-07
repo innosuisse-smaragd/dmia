2054f8c1ac2b5f5f9b0f4eb0f97799d612e86639
Thomas Jeanmonod <thomas.jeanmonod@mimacom.com>
2024-03-07 10:46:29 +0100

    use QuestionnaireService in QuestionnaireRestController

--- src/main/java/org/bfh/smaragd/dmia/controller/QuestionnaireRestController.java
+++ src/main/java/org/bfh/smaragd/dmia/controller/QuestionnaireRestController.java
@@ -1,44 +1,34 @@
 package org.bfh.smaragd.dmia.controller;
 
-import com.fasterxml.jackson.annotation.JsonAutoDetect;
-import com.fasterxml.jackson.databind.DeserializationFeature;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.introspect.VisibilityChecker;
+import lombok.AllArgsConstructor;
 import org.bfh.smaragd.dmia.domain.questionnaire.Questionnaire;
-import org.bfh.smaragd.dmia.domain.task.Task;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.core.io.Resource;
+import org.bfh.smaragd.dmia.service.QuestionnaireService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.io.IOException;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/questionnaires")
+@AllArgsConstructor
 public class QuestionnaireRestController {
 
-    @Value("classpath:data/questionnaire.json")
-    private Resource taskResource;
+    private QuestionnaireService questionnaireService;
 
 
     @GetMapping("/{id}")
-    public Questionnaire findBy(@PathVariable String id) {
-        return loadQuestionnaire();
+    public ResponseEntity<Questionnaire> findBy(@PathVariable String id) {
+        var oQuestionnaire = questionnaireService.findById(id);
+        return ResponseEntity.of(oQuestionnaire);
     }
 
-    private Questionnaire loadQuestionnaire() {
-        ObjectMapper mapper = new ObjectMapper();
-        mapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
-        mapper.setVisibility(VisibilityChecker.Std.defaultInstance().withFieldVisibility(JsonAutoDetect.Visibility.ANY));
-        Questionnaire questionnaire = null;
-        try {
-            questionnaire = mapper
-                    .readValue(taskResource.getFile(), Questionnaire.class);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return questionnaire;
+    @GetMapping
+    public List<Questionnaire> findAll() {
+        return questionnaireService.findAll();
     }
+
+
 }
