eb180519bdeba7c052963c69b83557484705a1b1
Thomas Jeanmonod <thomas.jeanmonod@mimacom.com>
2024-03-12 10:50:36 +0100

    EnableConfigurationProperties in DmiaApplication

--- src/main/java/org/bfh/smaragd/dmia/DmiaApplication.java
+++ src/main/java/org/bfh/smaragd/dmia/DmiaApplication.java
@@ -2,9 +2,11 @@ package org.bfh.smaragd.dmia;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
+@EnableConfigurationProperties
 public class DmiaApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
