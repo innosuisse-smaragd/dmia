e75e01a267025aed62a126ee84eb770842b856f7
Thomas Jeanmonod <thomas.jeanmonod@mimacom.com>
2024-03-13 07:54:25 +0100

    implement send response and update task

--- src/main/java/org/bfh/smaragd/dmia/repository/QuestionnaireRepository.java
+++ src/main/java/org/bfh/smaragd/dmia/repository/QuestionnaireRepository.java
@@ -1,15 +1,40 @@
 package org.bfh.smaragd.dmia.repository;
 
 import lombok.AllArgsConstructor;
+import lombok.Data;
 import org.bfh.smaragd.dmia.domain.questionnaire.Questionnaire;
 import org.springframework.stereotype.Repository;
 
+import java.util.*;
+
 @Repository
 @AllArgsConstructor
-public class QuestionnaireRepository extends UsernameLookupRepository<Questionnaire> {
+public class QuestionnaireRepository  {
 
-    @Override
-    public String extractId(Questionnaire entity) {
-        return entity.getId();
+    private final Map<QuestionnaireKey, Questionnaire> entities = new HashMap<>();
+
+    public void save(String username, String taskId, Questionnaire questionnaire) {
+        var id = questionnaire.getId();
+        var key = new QuestionnaireKey(username, taskId, id);
+        entities.put(key, questionnaire);
+    }
+
+    public List<Questionnaire> findByUsernameAndTaskId(String username, String taskId) {
+        if (username != null && taskId != null) {
+            return entities
+                    .entrySet()
+                    .stream()
+                    .filter(key -> username.equals(key.getKey().getUsername()) && taskId.equals(key.getKey().getTaskId()))
+                    .map(Map.Entry::getValue)
+                    .toList();
+        }
+        return Collections.emptyList();
+    }
+
+    @Data
+    private static class QuestionnaireKey {
+        private final String username;
+        private final String taskId;
+        private final String questionnaireId;
     }
 }
--- src/main/java/org/bfh/smaragd/dmia/repository/TaskRepository.java
+++ src/main/java/org/bfh/smaragd/dmia/repository/TaskRepository.java
@@ -4,14 +4,59 @@ import lombok.AllArgsConstructor;
 import org.bfh.smaragd.dmia.domain.task.Task;
 import org.springframework.stereotype.Repository;
 
+import java.util.*;
+
 
 @Repository
 @AllArgsConstructor
-public class TaskRepository extends UsernameLookupRepository<Task> {
+public class TaskRepository {
 
-    @Override
-    public String extractId(Task task) {
-        return task.getId();
+    //Key: username
+    //Value: set of ids
+    private final Map<String, Set<String>> usernameLookup = new HashMap<>();
+
+    private final Map<String, Task> tasks = new HashMap<>();
+
+    public void save(String username, Task task) {
+        var id = task.getId();
+        tasks.put(id, task);
+        updateUsernameLookup(username, id);
+    }
+
+    private void updateUsernameLookup(String username, String id) {
+        var ids = usernameLookup.getOrDefault(username, new HashSet<>());
+        ids.add(id);
+        usernameLookup.put(username, ids);
+    }
+
+    public Optional<Task> findByUsernameAndId(String username, String id) {
+        Task task = null;
+        if (username != null && id != null) {
+            boolean idBelongToUsername = usernameLookup.getOrDefault(username, Collections.emptySet()).contains(id);
+            if (idBelongToUsername) {
+                task = tasks.get(id);
+            }
+        }
+        return Optional.ofNullable(task);
+    }
+
+    public List<Task> findByUsername(String username) {
+        List<Task> results = Collections.emptyList();
+        if (username != null) {
+            results = usernameLookup
+                    .getOrDefault(username, Collections.emptySet())
+                    .stream()
+                    .map(tasks::get)
+                    .toList();
+        }
+        return results;
+    }
+
+    public void remove(String username, String id) {
+        findByUsernameAndId(username, id).ifPresent(entity -> {
+            usernameLookup.getOrDefault(username, Collections.emptySet()).remove(id);
+            tasks.remove(id);
+        });
     }
 
 
