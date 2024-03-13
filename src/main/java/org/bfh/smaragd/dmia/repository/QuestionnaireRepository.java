60a16682f5253d9ac6085c25f6ab5fba3292920b
Thomas Jeanmonod <thomas.jeanmonod@mimacom.com>
2024-03-13 08:49:55 +0100

    refactor QuestionnaireRepository

--- src/main/java/org/bfh/smaragd/dmia/repository/QuestionnaireRepository.java
+++ src/main/java/org/bfh/smaragd/dmia/repository/QuestionnaireRepository.java
@@ -1,40 +1,38 @@
 package org.bfh.smaragd.dmia.repository;
 
 import lombok.AllArgsConstructor;
-import lombok.Data;
 import org.bfh.smaragd.dmia.domain.questionnaire.Questionnaire;
 import org.springframework.stereotype.Repository;
 
-import java.util.*;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Repository
 @AllArgsConstructor
-public class QuestionnaireRepository  {
+public class QuestionnaireRepository {
 
-    private final Map<QuestionnaireKey, Questionnaire> entities = new HashMap<>();
+    private final Map<QuestionnaireKey, Questionnaire> questionnaires = new HashMap<>();
 
     public void save(String username, String taskId, Questionnaire questionnaire) {
         var id = questionnaire.getId();
         var key = new QuestionnaireKey(username, taskId, id);
-        entities.put(key, questionnaire);
+        questionnaires.put(key, questionnaire);
     }
 
     public List<Questionnaire> findByUsernameAndTaskId(String username, String taskId) {
         if (username != null && taskId != null) {
-            return entities
+            return questionnaires
                     .entrySet()
                     .stream()
-                    .filter(key -> username.equals(key.getKey().getUsername()) && taskId.equals(key.getKey().getTaskId()))
+                    .filter(key -> username.equals(key.getKey().username()) && taskId.equals(key.getKey().taskId()))
                     .map(Map.Entry::getValue)
                     .toList();
         }
         return Collections.emptyList();
     }
 
-    @Data
-    private static class QuestionnaireKey {
-        private final String username;
-        private final String taskId;
-        private final String questionnaireId;
+    private record QuestionnaireKey(String username, String taskId, String questionnaireId) {
     }
 }
