c86cf8237e26b41762fa4c6c55465f538839ede6
Damien Papaux <damien.papaux@mimacom.com>
2024-03-14 12:02:17 +0100

    add redirect controller in testing branch

---
+++ src/main/java/org/bfh/smaragd/dmia/controller/RedirectController.java
@@ -0,0 +1,18 @@
+package org.bfh.smaragd.dmia.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class RedirectController {
+
+    /*
+     * Redirects all routes to FrontEnd except: '/', '/index.html', '/api', '/api/**'
+     */
+    @RequestMapping(value = "{_:^(?!index\\.html|api).*$}")
+    public String redirect() {
+        return "forward:/";
+    }
+}
+
